3317ec982c934b0ebc609aed931d85e584f467e9
chan9yu <dev.cgyeo@gmail.com>
2025-01-15 21:13:37 +0900

    feat: back-end cors 설정

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const logger = require('morgan');
 const path = require('path');
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const cors = require('cors');
 
 const apiRouter = require('./routes/api');
 const indexRouter = require('./routes/index');
@@ -29,6 +30,7 @@ const openapiSpec = swaggerJsdoc({
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpec));
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
